34e44ebccd79796b817cffa389f74c84c2d2139f
George Roots <A73740@CITMBP-002.local>
2015-09-02 14:54:54 -0400

    added bootstrap. Created a header template for index.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
-
+var http = require('http');
 var app = express();
+
+// var options = {
+// 	http : 'http://github.com',
+// 	path: 'username'
+// }
 
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 3000);
